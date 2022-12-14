314654c712059f8d7645bd29f99b43a0327a41b9
장수현 <76679995+suhyeon10@users.noreply.github.com>
2022-12-14 21:44:44 +0900

    과일 장수

---
+++ Programmers/PMG_135808.java
@@ -0,0 +1,19 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class PMG_135808 {
+    public static void main(String[] args) {
+        System.out.println(solution(3, 4, new int[]{1,2,3,1,2,3,1}));
+        System.out.println(solution(4, 3, new int[]{4,1,2,2,4,4,4,4,1,2,4,2}));
+
+    }
+    public static int solution(int k, int m, int[] score){
+        int answer = 0;
+        Arrays.sort(score);
+        for( int i = score.length%m; i<score.length; i+=m){
+            answer+=score[i]*m;
+        }
+        return answer;
+    }
+}
