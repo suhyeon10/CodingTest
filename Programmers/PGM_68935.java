9d8250059a0fe579838caf4f1784a103af0d86d6
장수현 <76679995+suhyeon10@users.noreply.github.com>
2023-01-27 17:23:18 +0900

    3진법 뒤집기

---
+++ Programmers/PGM_68935.java
@@ -0,0 +1,25 @@
+package Programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static Programmers.PGM_131705.Solution.temp;
+
+public class PGM_68935 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int n){
+        int answer = 0;
+
+        String intList = "";
+        while(n!=0){
+            intList += n%3;
+            n/=3;
+        }
+
+        answer = Integer.parseInt(intList, 3);
+        return answer;
+    }
+}
