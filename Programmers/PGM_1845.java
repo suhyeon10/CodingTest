3c339adaa4bf8a5b2575e6ca4e5626a7310ed4a0
장수현 <76679995+suhyeon10@users.noreply.github.com>
2023-11-14 20:29:47 +0900

    의상

---
+++ Programmers/PGM_1845.java
@@ -0,0 +1,19 @@
+package Programmers;
+
+import java.util.HashSet;
+
+public class PGM_1845 {
+    public int solution(int[] nums) {
+        //해시셋 추가
+        HashSet<Integer> numSet = new HashSet<>();
+        for(int num : nums){
+            numSet.add(num);
+        }
+
+        int max = nums.length/2;
+
+
+        if(max>numSet.size()) return numSet.size();
+        else return max;
+    }
+}
